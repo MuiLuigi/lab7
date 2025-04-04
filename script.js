e5d119d59c94471b2a5a01aeb2bb2e905160aeaf
Maruigi <fahadmui123@gmail.com>
2025-04-04 05:02:36 -0400

    Completed Keyboard Event

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const button = document.getElementById('mouse');
 const message = document.getElementById('message');
 
 button.addEventListener('mouseenter', function() {
-            message.textContent = 'Hovering over button';
-        });
+        message.textContent = 'Hovering over button';
+    });
 button.addEventListener('mouseleave', function() {
-            message.textContent = 'Not hovering over button';
-        });
+        message.textContent = 'Not hovering over button';
+    });
 
 //Keyboard Event
+const keys = document.getElementById('keys');
+const show = document.getElementById('show');
+
+keys.addEventListener('keydown', function(keyDisplay) {
+    show.textContent = keyDisplay.key;
+});
+
+//Form Event
